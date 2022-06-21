40f8d41a22a15c5b3b2f6bcffe95000cc8e54cf3
Braden Rich <bradenwrich@gmail.com>
2022-06-21 09:18:11 -0400

    Add Game Manager functionality and tests

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -1,22 +1,89 @@
 # frozen_string_literal: true 
 
 require_relative 'game'
+require_relative 'round_result'
+require_relative 'player'
+require 'pry'
 
 class GameManager
-
-  attr_accessor :game, :sockets, :names
+  attr_accessor :sockets, :players, :game, :associated_list, :server, :first_message_sent, :asked_player, :rank, :ask_rank_message_sent
   def initialize(sockets: [], names: [])
+    @server = server
     @sockets = sockets
-    @names = names
-    @players = convert_to_players(names)
+    @players = create_players(names)
     @game = Game.new(players: @players)
+    @associated_list = associate_player_socket(sockets, @players)
+    @first_message_sent = false
+    @ask_rank_message_sent = false
+  end
+
+  def run_game
+    until game.over?
+      process_input
+      # returns round result
+    end
+  end
+
+  def process_input
+    beginning_message unless first_message_sent
+    self.asked_player ||= get_input_from_current_player
+    return unless asked_player
+    current_player_socket.puts "What rank would you like to ask for?" unless ask_rank_message_sent
+    self.ask_rank_message_sent = true 
+    self.rank ||= get_input_from_current_player
+    return unless rank
+    game.play_round(rank, game.get_player(asked_player))
+    reset_message_status
+  end
+
+  def beginning_message
+    @associated_list.each_key {|socket| socket.puts("Here's your hand!")}
+    @associated_list.each_pair {|socket, player| socket.puts(player.hand.map(&:to_s))}
+    current_player_socket.puts "It's your turn!\nWho would you like to ask for cards?"
+    send_to_all_except_current("It's #{current_player_name}'s turn. Go #{current_player_name}!\n")
+    self.first_message_sent = true 
+  end
+
+  def reset_message_status
+    self.first_message_sent = false
+    self.ask_rank_message_sent = false
+    return_output
+  end
+
+  def return_output
+    # TODO: access Round Result and print it
+  end
+
+  def get_input_from_current_player
+    sleep(0.01)
+    current_player_socket.read_nonblock(1000).chomp
+  rescue IO::WaitReadable
+  end
+
+  def send_to_all_except_current(message)
+    sockets.reject{|socket| socket == current_player_socket }.each {|socket| socket.puts(message)}
   end
 
   def start
-    game.start
+    game.start 
   end
 
-  def convert_to_players(names)
-    names.map {|name| Player.new(name: name)}
+  private
+  
+  def current_player_socket
+    @associated_list.key(game.turn_player)
+  end
+
+  def current_player_name
+    game.turn_player.name
+  end
+
+  def create_players(player_names)
+    player_names.map {|name| Player.new(name: name)}
+  end
+
+
+  def associate_player_socket(sockets, player_names)
+    sockets.map.with_index { |socket, index| [socket, player_names[index]] }.to_h
   end
 end
--- spec/game_manager_spec.rb
+++ spec/game_manager_spec.rb
@@ -19,10 +19,53 @@ require 'server'
   
     describe '#initialize' do
       it 'initializes without error when provided no clients or players' do
-        manager = GameManager.new
+        manager = GameManager.new()
+        expect(manager.sockets).to be_empty
+        expect(manager.players).to be_empty
         expect(manager.game.class).to eq Game
         expect(manager.game.players).to be_empty
         expect{ manager }.not_to raise_error
+      end
+    
+      it 'associates players and clients correctly' do
+        manager = GameManager.new(sockets: [TCPSocket.new('localhost', 3000)], names: ['Braden'])
+        expect(manager.sockets).to be_one
+        expect(manager.players).to be_one
+        expect(manager.associated_list.length).to eq(1)
+      end
+    end
+
+    describe '#process_input' do
+      it 'runs a round and sends the results to the players' do
+        clients = setup_server_with_clients(['Caleb', 'Braden'])
+        manager = @server.create_game_manager_if_possible
+        manager.start
+        manager.process_input
+        manager.associated_list.values[0].hand = [Card.new('A', 'S')]
+        manager.associated_list.values[1].hand = [Card.new('A', 'H')]
+        message = clients[0].read_from_server
+        expect(message).to include("It's your turn!")
+        expect(message).to include("Here's your hand!")
+        expect(message).to include("Who would you like to ask for cards?")
+        clients[0].send_to_server('Braden')
+        manager.process_input
+        expect(clients[0].read_from_server).to include("What rank would you like to ask for?")
+        clients[0].send_to_server('A')
+        manager.process_input
+        round_result = clients[0].read_from_server
+        expect(round_result).to include("Caleb took A's from Braden.")
+        expect(clients[1].read_from_server).to include("Caleb took your A's!")
+      end
+    end
+
+    def setup_server_with_clients(player_names)
+      player_names.map do |player_name|
+        client = Client.new(3000)
+        @server.accept_new_client
+        client.send_to_server(player_name)
+        @server.get_player_name
+        client.read_from_server
+        client
       end
     end
 end
